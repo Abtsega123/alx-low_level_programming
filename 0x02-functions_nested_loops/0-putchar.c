2b9a98b31a4a7ca723b895f1be29db364b457c45
Abtsega123 <andyboringo1@gmail.com>
2022-03-16 00:58:57 +0530

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * main - entry block
- * @void: no argument
- * Return: 0
- **/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char alx[] = "_putchar";
+	char *sh = "_putchar";
 
-	int c;
-
-	for (c = 0; c < 8; c++)
+	while (*sh)
 	{
-		_putchar(alx[c]);
+		_putchar(*sh);
+		sh++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
