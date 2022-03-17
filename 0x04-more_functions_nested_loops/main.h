7169c88941c69796fc7755294d03115b27afdd71
Abtsega123 <andyboringo1@gmail.com>
2022-03-17 23:21:20 +0530

    comited

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-char _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int nld);
-void jack_bauer(void);
-void times_table(void);
-int add(int one, int two);
-void print_to_98(int n);
-void print_times_table(int n);
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-more_functions_nested_loops directory.
+ */
 
-#endif /* MAIN_H *
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
