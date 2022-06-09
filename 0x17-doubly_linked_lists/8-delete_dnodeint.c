322039993aa477d6a0d1a215ac317e00ccfa7ff4
Abtsega123 <andyboringo1@gmail.com>
2022-06-10 03:14:13 +0530

    commited

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,40 +1,68 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes a node at a specific index
- * @head: double pointer to the linked list
- * @index: index at which to delete node
- *
- * Return: 1 on success, -1 on failure
- */
+  * delete_dnodeint_at_index - deletes node at index index of list.
+  * @head: double pointer to list.
+  * @index: index of node to be deleted.
+  *
+  * Return: 1 if it succeeded, -1 if it failed.
+  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current;
-	unsigned int i;
+	dlistint_t *temp, *temp2, *tp;
+	unsigned int len = dlistint_len2(*head);
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL || len < index)
 		return (-1);
-	current = *head;
-	if (index == 0)
+	temp = *head;
+	if (index == 0 && (*head)->next == NULL)
 	{
-		*head = current->next;
-		if (current->next != NULL)
-		{
-			current->next->prev = NULL;
-		}
-		free(current);
+		free(temp), *head = NULL;
 		return (1);
 	}
-	for (i = 0; i < index; i++)
+	if (index ==  0 && (*head)->next != NULL)
 	{
-		if (current->next == NULL)
-			return (-1);
-		current = current->next;
+		*head = (*head)->next;
+		(*head)->prev = NULL;
 	}
-	current->prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-	free(current);
+	if (index > 0)
+	{
+		while (index != 0)
+			temp = temp->next, index--;
+		if (temp->next == NULL)
+		{
+			tp = *head;
+			while (tp->next != NULL)
+				tp = tp->next;
+			temp2 = tp->prev;
+			temp2->next = NULL, free(tp);
+			return (1);
+		}
+		temp2 = temp->prev;
+		temp2->next = temp->next;
+		temp->next->prev = temp2;
+	}
+	free(temp);
 	return (1);
+}
+
+/**
+  * dlistint_len2 - returns number of elements in a lineked dlistint_t list.
+  * @h: pointer to list.
+  *
+  * Return: number of elements in list.
+  */
+unsigned int dlistint_len2(dlistint_t *h)
+{
+	dlistint_t *temp;
+	unsigned int count;
+
+	temp = h;
+	count = 0;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count++;
+	}
+	return (count);
 }
