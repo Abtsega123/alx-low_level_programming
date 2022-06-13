72548bba0c09ea081be37f7da052d7732d6213e3
Abtsega123 <andyboringo1@gmail.com>
2022-06-13 21:15:41 +0530

    commited

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/libtest.so https://github.com/MitaliSengupta/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.so
+wget -P .. https://raw.githubusercontent.com/davidmusau/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
